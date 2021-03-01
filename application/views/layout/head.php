e65ce1b26448d07d92c422608c0523f7ff3b3563
Natanael <natanaela.neto@unifacol.edu.br>
2021-02-28 22:03:01 -0300

    ajuste no href fontawesome

--- application/views/layout/head.php
+++ application/views/layout/head.php
@@ -5,7 +5,7 @@
 <!-- Google Font: Source Sans Pro -->
 <link rel="stylesheet" type="text/css" href="<?php echo base_url() . "public/css/bootstrap.min.css "?>">
 <link rel="stylesheet" type="text/css" href="<?php echo base_url() . "public/css/mystyle.css "?>">
-<link href="./public/fontawesome/css/all.css" rel="stylesheet">
+<link href="<?= base_url('public/FontAwesome/css/all.css')?>" rel="stylesheet">
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Teko:wght@300;500;600;700&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;1,400&display=swap" rel="stylesheet">
