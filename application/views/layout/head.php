b79aadffccf9756346d7ae5f0fa23e90a04b9a56
Anna Maria <aannamariabr@outlook.com>
2021-03-01 00:26:12 -0300

    Icone do site

--- application/views/layout/head.php
+++ application/views/layout/head.php
@@ -7,6 +7,7 @@
 <link rel="stylesheet" type="text/css" href="<?php echo base_url() . "public/css/mystyle.css "?>">
 <link href="./public/fontawesome/css/all.css" rel="stylesheet">
 <link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Teko:wght@300;500;600;700&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;1,400&display=swap" rel="stylesheet">
+<link rel="shortcut icon" type="imagex/png" href="public/imagens/icon.ico">
+
 
