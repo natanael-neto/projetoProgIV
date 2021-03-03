08347bbb77798633806fc2923884a26fea3c7906
Anna Maria <aannamariabr@outlook.com>
2021-03-02 23:59:57 -0300

    View Aluno

---
+++ application/views/aluno.php
@@ -0,0 +1,22 @@
+<?php include APPPATH . 'views\layoutElements\navAluno.php' ?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-12 MensagemInicio">
+            <h1>Olá <strong>Anna Maria</strong></h1>
+        </div>
+    </div>
+
+    <div class="row AlunoPressets">
+        <div class="col-md ContainerDados">
+            <h2>Seus dados</h2>
+            <h5>Nome: Anna Maria</h5>
+            <h5>CPF: 111.111.111-11</h5>
+            <h5>Idade: 18</h5>
+        </div>
+        <div class="col-md ContainerAgendamento">
+            <h2>Agende suas aulas</h2>
+            <h5>Escolha o dia: </h5>
+        </div>
+    </div>
+</div>
